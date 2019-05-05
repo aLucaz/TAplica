0fb6068bd5c6509140a6cdca0e99495c7ec34f4a
Dan <jhairguz.t@gmail.com>
2019-05-05 10:44:05 -0500

    print function in test deleted
    no good practice to test

--- Part1/test/searches.test.py
+++ Part1/test/searches.test.py
@@ -7,6 +7,5 @@ from Part1.searchs.best_first_graph_search import astar_search
 
 if __name__ == "__main__":
     pittsMap = create_map()
-    print(pittsMap.heuristics['1'])
     pittsProblem = MapSearchProblem('104878620', '105012740', pittsMap)
     solNode, exploredNodes = astar_search(pittsProblem)
