8e41241d244879ab23825f7fdd1e4a510098297a
heisenberg098 <arturolazlo@outlook.es>
2019-05-05 15:11:01 -0500

    no c mano gg

--- Part1/main.py
+++ Part1/main.py
@@ -7,6 +7,7 @@ import click
 @click.option('-g', required=True, type=int, help="id de nodo objetivo")
 @click.option('-m', required=True, type=click.Choice(['bfs','dfs','ids','bis','astar']), help="método de búsqueda")
 @click.option('-h', type=click.File('r'), help="archivo de información heurística")
+
 def run(streets_file, id_init, id_goal, method, heuristic_file):
     # print("streets file  : ", streets_file)
     print("node id init  : ", id_init)
